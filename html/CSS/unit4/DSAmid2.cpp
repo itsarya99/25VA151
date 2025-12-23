98fa34715bd98c838e321cfb3aa8dc96d7cc4be3
arya shukla <aryashukla5859@gmail.com>
2025-12-23 16:24:35 +0530

    gradient

---
+++ html/CSS/unit4/DSAmid2.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // int p;
+    // cout<<"no of digits";
+    // cin>>p;
+
+    // int n;
+    // cout<<"enter n";
+    // cin>>n;
+
+    // int original =n;
+    // long result =0;
+
+    // while(n>0){
+    //     int d=n%10;
+    //     result+=(pow(d,p));
+    //     n/10;
+
+    
+    // }
+    // if(result==original){
+    //     cout<<"armstrong";
+    // }
+    // else{
+    //     cout<<"not amrstrong";
+    // }
+    // return 0;
+
+//     int n;
+//      cin>>n;
+//     int a[n][n];
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for(int j=0;j<n;j++)
+//         {
+//             cin>>a[i][j];
+//         }
+//     }
+
+//     int sum=0;
+
+//     for(int i =0;i<n;i++){
+//         sum+=a[i][i];
+//     }
+//     cout<<"sum of element"<<" "<<sum<<endl;
+//     return 0;
+    
+// }
+
+int n;
+cin>>n;
+if((n==1)){
+    cout<<"1 is prime";
+}
+else if{
+    for(int i=2;i<n/2;i++){
+        if(n%i==0);
+        cout<<"is prime";
+
+    }else{
+        cout<<"not prime";
+    }
+}
